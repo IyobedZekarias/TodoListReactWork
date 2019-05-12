447ff47ba4490e7f53e995f6970a3c7d750d1952
Iyobed Zekarias <IyobedZekarias@users.noreply.github.com>
2019-05-12 16:49:38 -0500

    Updates

---
+++ precache-manifest.0ff0d212c5bf29ede244bd37c0c328c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f122379d507eeb130b4589f2dbd300",
+    "url": "/TodoListReactWork/index.html"
+  },
+  {
+    "revision": "b6ed3accf1d56139363f",
+    "url": "/TodoListReactWork/static/css/main.a209ff59.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/TodoListReactWork/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b6ed3accf1d56139363f",
+    "url": "/TodoListReactWork/static/js/main.9069f3d0.chunk.js"
+  },
+  {
+    "revision": "9dd28667f9d8f3f6fc81",
+    "url": "/TodoListReactWork/static/js/runtime~main.11c0f32d.js"
+  }
+]);
